c40da03e484847abf49251f5c490469a1dabc3cc
samatt14 <54123654+samatt14@users.noreply.github.com>
2024-01-03 19:33:03 -0600

    Create 7_ReverseInteger.cpp

---
+++ 7_ReverseInteger.cpp
@@ -0,0 +1,47 @@
+#include <queue>
+#include <cmath>
+
+class Solution {
+public:
+    int reverse(int x) {
+        queue<int> q;
+
+        if(x < 0){
+            if(x == -2147483648){
+                return 0;
+            }
+            x*=-1;
+            while(x > 0){
+                q.push(x%10);
+                x = x/10;
+            }
+
+            while (!q.empty()) {
+                if( ((pow(2,31) - 1)/10) < x){
+                    return 0;
+                }
+                x = x*10;
+                x += q.front();
+                q.pop();
+            }
+
+            return x*=-1;
+        }
+
+        while(x > 0){
+            q.push(x%10);
+            x = x/10;
+        }
+
+        while (!q.empty()) {
+            if( ((pow(2,31) - 1)/10) < x){
+                return 0;
+            }
+            x = x*10;
+            x += q.front();
+            q.pop();
+        }
+
+        return x;
+    }
+};
